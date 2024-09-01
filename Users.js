b2bb5779b07f0b82f7c6665161df8dd097994af4
Shaileshkumar01 <153973933+Shaileshkumar01@users.noreply.github.com>
2024-09-01 16:06:27 +0530

    Create Users.js

---
+++ Users.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+
+// Register a new user
+router.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+// Get a user by ID
+router.get('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.send(user);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+module.exports = router;
